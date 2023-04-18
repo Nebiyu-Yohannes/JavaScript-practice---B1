6ffa37b4443b3481e1a620dfe90f6b9651495b4c
neba2020NF7 <96549723+neba2020NF7@users.noreply.github.com>
2023-04-18 12:29:35 +0300

    trial

--- an_imported_fun.js
+++ an_imported_fun.js
@@ -12,3 +12,9 @@ let num1 = 10; let num2 = 20;
 
 console.log(sum(num1, num2));
 console.log(sub(num1, num2));
+
+console.log(sum(num1, num2));
+console.log(sub(num1, num2));
+
+console.log(sum(33, 45));
+console.log(sub(2, 65));
