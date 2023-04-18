aa7fe4b700c83e96490cf3528d6f9c4cac5cc052
neba2020NF7 <96549723+neba2020NF7@users.noreply.github.com>
2023-04-18 12:16:26 +0300

    trial1

--- an_imported_fun.js
+++ an_imported_fun.js
@@ -1,6 +1,14 @@
 // trying to import a simple function 
 
+// this are what we are importing from the exported_fun file
 //import * as FirstOrder from '../exported_fun'
-import { sum } from "exported_fun.js";
+import { sum } from "exported_fun";
+import { num1 } from "exported_fun";
+import { num2 } from "exported fun";
+import { sub } from "exported_fun";
 
-console.log(sum(10, 20));
+
+let num1 = 10; let num2 = 20;
+
+console.log(sum(num1, num2));
+console.log(sub(num1, num2));
--- exported_fun.js
+++ exported_fun.js
@@ -1,3 +1,12 @@
 // creating an adding function to be exported 
 
+export var num1;
+export var num2;
+
 export const sum = (num1, num2) =>  num1 + num2;
+
+// creating a subtracting function to be exported
+
+export function sub (num1, num2){
+    return num1 - num2;
+} 
